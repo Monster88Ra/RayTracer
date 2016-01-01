a3a29b2aa07ace75e3d49ad7dc275afa6025d045
wang luowei <luowei.wang@outlook.com>
2016-01-01 22:23:27 +0800

    + init the project and add Vector2

---
+++ trunk/RayTracer/RayTracer/TestVec2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "vector2.h"
+
+int main()
+{
+	Vector2i a(1);
+	Vector2i b(2, 2);
+
+
+	Vector2i sum = a + b;
+	sum.Print();
+
+	std::cout << std::endl;
+
+	Vector2i minus = b - a;
+	minus.Print();
+
+	std::cout << std::endl;
+
+	sum = sum * 2;
+	sum.Print();
+
+	std::cout << std::endl;
+
+	sum /= 2;
+	sum.Print();
+
+	std::cout << std::endl;
+	std::cout << " a equal b" << (a == b) << std::endl;
+	std::cout << " a not equal b" << (a != b) << std::endl;
+
+	std::cout << "x is  " << sum[0] << std::endl;
+
+	std::cout << "the length 2 is " << sum.Length2() << std::endl;
+
+	std::cout << "the length is " << sum.Length() << std::endl;
+
+	std::cout << "normalize sum is ";
+	sum.Normalize();
+	sum.Print();
+
+	std::cout << std::endl << "a dot b" << Vector2i::Dot(a, b) << std::endl;
+	return 0;
+}
---
+++ trunk/RayTracer/RayTracer/include/math/Vector2.h
@@ -0,0 +1,237 @@
+#ifndef _VECTOR2_H
+#define _VECTOR2_H
+
+#include <cstdint>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+/*****************************
+ * Class of the 2D vector.
+******************************/
+template <typename T>
+class Vector2
+{
+public:
+	Vector2();
+	Vector2(T X);
+	Vector2(T X, T Y);
+	~Vector2();
+	
+	template<typename U> 
+	Vector2<T>& operator *= (const U &scalar);
+	Vector2<T>& operator += (const Vector2<T> &rhs);
+	Vector2<T>& operator -= (const Vector2<T> &rhs);
+	template<typename U> 
+	Vector2<T>& operator /= (const U &scalar);
+	bool operator == (const Vector2<T> &rhs) const;
+	bool operator != (const Vector2<T> &rhs) const;
+	T& operator[] (std::size_t index);
+	const T& operator[] (std::size_t index) const;
+
+	void Normalize();
+	float Length();
+	// square of length
+	float Length2();
+	Vector2<T> Reflect(const Vector2<T> &normal) const;
+	static T Dot(const Vector2<T> &lhs, const Vector2<T> &rhs);
+	void Print();
+public:
+	T x;
+	T y;
+};
+
+// c++ 11
+using Vector2i = Vector2<int32_t>;
+using Vector2f = Vector2<float>;
+
+/*****************************
+ * Inlined Member Functions 
+ ******************************/
+template <typename T>
+Vector2<T>::Vector2():
+	x(0), y(0)
+{
+
+}
+
+template <typename T>
+Vector2<T>::Vector2(T X) :
+x(X), y(X)
+{
+
+}
+
+template <typename T>
+Vector2<T>::Vector2(T X, T Y) :
+x(X), y(Y)
+{
+
+}
+
+template <typename T>
+Vector2<T>::~Vector2()
+{
+
+}
+
+template <typename T>
+template<typename U> 
+inline Vector2<T>& Vector2<T>::operator *= (const U &scalar)
+{
+	x *= scalar;
+	y *= scalar;
+
+	return *this;
+}
+
+template <typename T> 
+inline Vector2<T>& Vector2<T>::operator += (const Vector2<T> &rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+
+	return *this;
+}
+
+template <typename T>
+inline Vector2<T>& Vector2<T>::operator -= (const Vector2<T> &rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+
+	return *this
+}
+
+template <typename T>
+template <typename U>
+inline Vector2<T>& Vector2<T>::operator /= (const U &scalar)
+{
+	x /= scalar;
+	y /= scalar;
+
+	return *this;
+}
+
+template <typename T>
+inline bool Vector2<T>::operator== (const Vector2<T> &rhs) const
+{
+	return (x == rhs.x && y == rhs.y);
+}
+
+template <typename T>
+inline bool Vector2<T>::operator!=(const Vector2<T> &rhs) const
+{
+	return (x != rhs.x || y != rhs.y);
+}
+
+template <typename T>
+T& Vector2<T>::operator[] (std::size_t index)
+{
+	switch (index)
+	{
+	case 0:
+		return x;
+	case 1:
+		return y;
+	default:
+		throw std::out_of_range("Vector2 subscript out of range.");
+	}
+}
+
+template <typename T>
+const inline T& Vector2<T>::operator[] (std::size_t index) const
+{
+	switch (index)
+	{
+	case 0:
+		return x;
+	case 1:
+		return y;
+	default:
+		throw std::out_of_range("Vector2 subscript out of range.");
+	}
+}
+
+template <typename T>
+inline float Vector2<T>::Length2()
+{
+	return x*x + y*y;
+}
+
+template <typename T>
+inline float Vector2<T>::Length()
+{
+	return std::sqrt(x*x + y*y);
+}
+
+template <typename T>
+inline void Vector2<T>::Normalize()
+{
+	float nor2 = Length2();
+	if (nor2 > 0)
+	{
+		float invNor = 1 / sqrt(nor2);
+		x *= invNor;
+		y *= invNor;
+	}
+}
+
+template <typename T>
+void Vector2<T>::Print()
+{
+	std::cout << "X:" << x << " " << "Y:" << y;
+}
+
+template <typename T>
+inline T Vector2<T>::Dot(const Vector2<T> &lhs,const Vector2<T> &rhs)
+{
+	return lhs.x * rhs.x + lhs.y * rhs.y;
+}
+
+/*****************************
+* Non-member Functions
+******************************/
+
+template <typename T>
+inline Vector2<T> operator + (const Vector2<T> &lhs, const Vector2<T> &rhs)
+{
+	return Vector2<T>(lhs.x + rhs.x, lhs.y + rhs.y);
+}
+
+template <typename T>
+inline Vector2<T> operator - (const Vector2<T> &lhs, const Vector2<T> &rhs)
+{
+	return Vector2<T>(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+template <typename T>
+inline Vector2<T> operator - (const Vector2<T> &lhs)
+{
+	return Vector2<T>(-lhs.x, -lhs.y);
+}
+
+template <typename T,typename U>
+inline Vector2<T> operator * (const Vector2<T> &vec, const U &scalar)
+{
+	return Vector2<T>(vec.x * scalar, vec.y * scalar);
+}
+
+template <typename T, typename U>
+inline Vector2<T> operator * (const U &scalar, const Vector2<T> &vec)
+{
+	return Vector2<T>(vec.x * scalar, vec.y * scalar);
+}
+
+template <typename T, typename U>
+inline Vector2<T> operator / (const Vector2<T> &vec, const U &scalar)
+{
+	return Vector2<T>(vec.x / scalar, vec.y / scalar);
+}
+
+template <typename T, typename U>
+inline Vector2<T> operator / (const U &scalar, const Vector2<T> &vec)
+{
+	return Vector2<T>(scalar/vec.x , scalar/vec.y );
+}
+#endif//_VECTOR2_H
